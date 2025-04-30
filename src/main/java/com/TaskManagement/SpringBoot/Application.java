5fc4885d469a6697b7d3a3d1ad0267cc216e373b
M.Alshamari <mo.2301@hotmail.com>
2025-04-30 12:51:52 +0300

    Update Test

--- src/main/java/com/TaskManagement/SpringBoot/Application.java
+++ src/main/java/com/TaskManagement/SpringBoot/Application.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		System.out.println(" Tesssst ");
 	}
 }
 
