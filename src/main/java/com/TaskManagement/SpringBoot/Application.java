96557ec1cda45f62df2515acec34e5a6363da102
M.Alshamari <mo.2301@hotmail.com>
2025-04-30 16:47:00 +0300

    this update for PC

--- src/main/java/com/TaskManagement/SpringBoot/Application.java
+++ src/main/java/com/TaskManagement/SpringBoot/Application.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@SpringBootApplication // 30-04-2025, 04:32 AM
+@SpringBootApplication // 30-04-2025, 04:46 PM
 @EnableMethodSecurity
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-		System.out.println("hhh");
+		System.out.println("this update for PC");
 	}
 }
 
